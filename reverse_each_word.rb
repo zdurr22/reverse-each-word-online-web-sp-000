732e98dd43ca6a1568fc66f9bf07ea40c113eb9b
Zachary Durr <zdurr22@gmail.com>
2020-10-01 17:48:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   words_array = sentence.split(" ")
   new_array = []
   words_array.collect do |word|
-  new_array << word.reverse
-end
+    new_array << word.reverse
+  end
 
 end
