b39f414bd9145f127e875664462037ff87dabd77
Zachary Durr <zdurr22@gmail.com>
2020-10-01 17:49:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(sentence)
   words_array = sentence.split(" ")
   new_array = []
-  words_array.collect do |word|
-    new_array << word.reverse
-  end
+  words_array.collect { |word| word.reverse }
 end
